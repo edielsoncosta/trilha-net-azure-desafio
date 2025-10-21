cb292e08d5e03182b5b103e4805c2cf63cba4dec
Edielson <edielsoncosta.c@gmail.com>
2025-10-20 23:03:54 -0300

    adicionando as solicitações nos todos

--- Program.cs
+++ Program.cs
@@ -3,25 +3,42 @@ using TrilhaNetAzureDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adiciona DbContext com SQL Server
 builder.Services.AddDbContext<RHContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
 
+// Adiciona controllers
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configura Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// **Configura CORS para permitir requisições do Swagger e outros clientes**
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
-// Swagger
+// Ativa Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Ativa CORS
+app.UseCors("AllowAll");
+
+// Redirecionamento HTTPS
 app.UseHttpsRedirection();
 
+// Autorização
 app.UseAuthorization();
 
+// Mapeia Controllers
 app.MapControllers();
 
 app.Run();
